54935125cb75be803bbe1ed7a0716796ae0c9a8e
Xuan Liu <liuxuan@Xuans-MacBook-Pro.local>
2019-01-01 23:38:35 -0800

    completed basic project0 ver2

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -10,7 +10,7 @@ public class Planet {
     public String imgFileName;
     // The name of the file that corresponds to the image that
     // depicts the planet
-    public static final double G = 6.67e-11; // gravitational constant
+    private static final double G = 6.67e-11; // gravitational constant
 
     public Planet(double xP, double yP, double xV,
                   double yV, double m, String img){
