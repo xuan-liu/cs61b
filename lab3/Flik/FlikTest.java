362b24390f7248235e473d181fe5efc28de2edf2
Xuan Liu <liuxuan@Xuans-MacBook-Pro.local>
2019-01-06 22:58:58 -0800

    completed lab3

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlikTest {
+    @Test
+    public void testIsSameNumber() {
+        assertEquals(true, Flik.isSameNumber(128,128));
+    }
+
+}
