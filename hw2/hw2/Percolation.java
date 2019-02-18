800e048b6a20a53ee8185d3d327b1a113588ac82
Xuan Liu <liuxuan@dhcp-v087-020.mobile.uci.edu>
2019-02-17 16:16:03 -0800

    completed hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,133 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[][] grid;
+    private WeightedQuickUnionUF wquWithHT;
+    private WeightedQuickUnionUF wquWithH;
+    private int numberOfOpen;
+    private int head;
+    private int tail;
+
+    /** create N-by-N grid, with all sites initially blocked */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException(
+                    "the input must greater than 0!");
+        }
+
+        numberOfOpen = 0;
+
+        grid = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = false;
+            }
+        }
+
+        wquWithHT = new WeightedQuickUnionUF(N * N + 2);
+        wquWithH = new WeightedQuickUnionUF(N * N + 1);
+        head = N * N;
+        tail = N * N + 1;
+    }
+
+    /** convert 2 dimentional array into 1 dimentional array */
+    private int xyTo1D(int r, int c) {
+        return r * grid.length + c;
+    }
+
+    /** check whether there are open neighbor, if yes, union with it */
+    private void checkUnion(int row, int col) {
+        int here = xyTo1D(row, col);
+        if (row > 0 && grid[row - 1][col] == true) {
+            int neigh = xyTo1D(row - 1, col);
+            wquWithHT.union(here, neigh);
+            wquWithH.union(here, neigh);
+        }
+        if (row < grid.length - 1 && grid[row + 1][col] == true) {
+            int neigh = xyTo1D(row + 1, col);
+            wquWithHT.union(here, neigh);
+            wquWithH.union(here, neigh);
+        }
+        if (col > 0 && grid[row][col - 1] == true) {
+            int neigh = xyTo1D(row, col - 1);
+            wquWithHT.union(here, neigh);
+            wquWithH.union(here, neigh);
+        }
+        if (col < grid.length - 1 && grid[row][col + 1] == true) {
+            int neigh = xyTo1D(row, col + 1);
+            wquWithHT.union(here, neigh);
+            wquWithH.union(here, neigh);
+        }
+    }
+
+    /** check for row, if at top of grid, union with head;
+     * if at bottom of grid, union with tail */
+    private void checkHeadTail(int row, int col) {
+        if (row == 0) {
+            wquWithHT.union(xyTo1D(row, col), head);
+            wquWithH.union(xyTo1D(row, col), head);
+        }
+        if (row == grid.length - 1) {
+            wquWithHT.union(xyTo1D(row, col), tail);
+        }
+    }
+
+    /** check whether the inout row and col is inside the grid range */
+    private void checkInput(int row, int col) {
+        if (!(row >= 0 && row <= grid.length - 1
+                && col >= 0 && col <= grid.length - 1)) {
+            throw new java.lang.IndexOutOfBoundsException(
+                    "the input row or column is not between 0 and n-1!");
+        }
+    }
+
+    /** is the site (row, col) open? */
+    public boolean isOpen(int row, int col) {
+        checkInput(row, col);
+        return grid[row][col];
+    }
+
+    /** open the site (row, col) if it is not open already */
+    public void open(int row, int col) {
+        checkInput(row, col);
+        if (!isOpen(row, col)) {
+            grid[row][col] = true;
+            numberOfOpen += 1;
+            checkUnion(row, col);
+            checkHeadTail(row, col);
+        }
+    }
+
+    /** is the site (row, col) full? */
+    public boolean isFull(int row, int col) {
+        checkInput(row, col);
+        return wquWithH.connected(xyTo1D(row, col), head);
+    }
+
+    /** number of open sites */
+    public int numberOfOpenSites() {
+        return numberOfOpen;
+    }
+
+    /** does the system percolate? */
+    public boolean percolates() {
+        return wquWithHT.connected(head, tail);
+    }
+
+    public static void main(String[] args) {
+//        Percolation test = new Percolation(5);
+//        test.open(3, 4);
+//        test.open(2, 4);
+//        System.out.println(test.wquWithHT.connected(14, 19));
+//        test.open(2, 2);
+//        test.open(2, 3);
+//        test.open(0, 2);
+//        System.out.println(test.isFull(0, 2));
+//        test.open(1, 2);
+//        System.out.println(test.isFull(2, 2));
+//        test.open(4, 4);
+//        System.out.println(test.percolates());
+//        System.out.println(test.numberOfOpenSites());
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,57 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+public class PercolationStats {
+    double[] threshold;
+
+    /** perform T independent experiments on an N-by-N grid */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException(
+                    "n and t must greater than 0!");
+        }
+
+        threshold = new double[T];
+
+        for (int i = 0; i < T; i++){
+            Percolation p = pf.make(N);
+
+            while(!p.percolates()) {
+                int rRow = StdRandom.uniform(N);
+                int rCol = StdRandom.uniform(N);
+                p.open(rRow, rCol);
+            }
+
+            threshold[i] = (double) p.numberOfOpenSites() / (N * N);
+        }
+
+    }
+
+    /** sample mean of percolation threshold */
+    public double mean() {
+        return StdStats.mean(threshold);
+    }
+
+    /** sample standard deviation of percolation threshold */
+    public double stddev() {
+        return StdStats.stddev(threshold);
+    }
+
+    /** low endpoint of 95% confidence interval */
+    public double confidenceLow() {
+        return mean() - 1.96 * Math.sqrt(stddev() / threshold.length);
+    }
+
+    /** high endpoint of 95% confidence interval */
+    public double confidenceHigh() {
+        return mean() + 1.96 * Math.sqrt(stddev() / threshold.length);
+    }
+
+    public static void main(String[] args) {
+//        PercolationFactory pf = new PercolationFactory;
+//        PercolationStats test = new PercolationStats(5,3);
+//        System.out.println(test.mean());
+    }
 }
